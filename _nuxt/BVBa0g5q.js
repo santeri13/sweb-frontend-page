ec8be27470a796cc2eb55e61431a6a44ae3d6a5d
Santeri Pohjaranta <49091889+santeri13@users.noreply.github.com>
2026-08-09 22:10:27 +0300

    Buisness and User pages union

---
+++ _nuxt/BVBa0g5q.js
@@ -0,0 +1,1 @@
+import{a4 as t}from"./Blsbch9z.js";const o=t("reservation",{state:()=>({id:0}),actions:{setReservationID(e){this.id=e}}});export{o as u};
---
+++ _nuxt/CldOaGBf.js
@@ -0,0 +1,1 @@
+import{a4 as t}from"./CcGTU6_0.js";const o=t("reservation",{state:()=>({id:0}),actions:{setReservationID(e){this.id=e}}});export{o as u};
